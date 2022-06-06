d9f809630e17f88eb80cf3d15b6602f0348b7eca
Patrick Krämer <patagonahn@gmail.com>
2022-06-06 14:33:35 +0200

    improve PathComparer performance

--- src/MusicSyncConverter/MusicSyncConverter.FileProviders/PathComparer.cs
+++ src/MusicSyncConverter/MusicSyncConverter.FileProviders/PathComparer.cs
@@ -6,42 +6,61 @@ namespace MusicSyncConverter.FileProviders
 {
     public class PathComparer : IEqualityComparer<string>
     {
-        private readonly bool _multipleSeparators;
+        private static readonly bool _multipleSeparators = Path.DirectorySeparatorChar != Path.AltDirectorySeparatorChar;
         private readonly StringComparer _internalComparer;
 
         public PathComparer(bool caseSensitive)
         {
-            _multipleSeparators = Path.DirectorySeparatorChar != Path.AltDirectorySeparatorChar;
             _internalComparer = caseSensitive ? StringComparer.Ordinal : StringComparer.OrdinalIgnoreCase;
         }
 
         public bool Equals(string? x, string? y)
         {
-            return _internalComparer.Equals(NormalizePath(x), NormalizePath(y));
+            if (_internalComparer.Equals(x, y))
+                return true;
+
+            if ((HasPathSeperator(x) || HasPathSeperator(y)) && _internalComparer.Equals(NormalizePath(x), NormalizePath(y)))
+                return true;
+            return false;
+        }
+
+        private bool HasPathSeperator(string? x)
+        {
+            return x != null && (x.Contains(Path.DirectorySeparatorChar) || (_multipleSeparators && x.Contains(Path.AltDirectorySeparatorChar)));
         }
 
         private string? NormalizePath(string? path)
         {
-            if(path == null)
+            if (path == null)
                 return null;
             var pathParts = path.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
-            var stack = new Stack<string>();
-            foreach (var part in pathParts)
+            var stack = new Stack<string>(pathParts.Length);
+            try
             {
-                if (part == "..")
+                foreach (var part in pathParts)
                 {
-                    stack.Pop();
-                }
-                else if (part == ".")
-                {
-                    continue;
-                }
-                else
-                {
-                    stack.Push(part);
+                    if (part == "..")
+                    {
+                        stack.Pop();
+                    }
+                    else if (part == ".")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        stack.Push(part);
+                    }
                 }
             }
-            return string.Join(Path.DirectorySeparatorChar, stack);
+            catch (InvalidOperationException ex)
+            {
+                throw new InvalidOperationException($"Invalid Path {path}", ex);
+            }
+
+            var partsArray = stack.ToArray();
+            Array.Reverse(partsArray);
+            return string.Join(Path.DirectorySeparatorChar, partsArray);
         }
 
         public int GetHashCode(string? obj)
